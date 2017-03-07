0d25c080fec0c71dad42b2ae5e281cb305f3fb0c
harshadodda <harshadodda@gmail.com>
2017-03-07 17:27:24 -0600

    Initial commit of the code folder
    Code folder for gem knights. No other assets are included

---
+++ Player/EnemyCollider.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyCollider : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	// for this to work, player must have a collider with the isTrigger set to true
+	void OnTriggerStay(Collider other) { // checks if player is in contact with enemy
+		if (other.gameObject.CompareTag ("Enemy") && Input.GetMouseButtonDown(0)) {
+			// the if statement checks if the colliding object is the Enemy 
+			// and if the player pressed the left mouse click 
+			other.gameObject.SetActive (false); // make the enemy disappear
+		}
+	}
+}
---
+++ Player/PlayerMovement.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // How fast the character can move
+    public int movementSpeed = 10;
+    public int rotateSpeed = 100;
+    public int jumpHeight = 400;
+
+    // Players RigidBody
+    private Rigidbody playerRB;
+    float distToGround;
+
+    void Start()
+    {
+        // Gets the RigidBody
+        playerRB = GetComponent<Rigidbody>();
+
+        //
+        distToGround = GetComponent<Collider>().bounds.extents.y;
+    }
+    void Update()
+    {
+
+        
+        // Move forward
+        if (Input.GetKey("w"))
+        {
+            transform.Translate(Vector3.forward * (Time.deltaTime * movementSpeed));
+
+        }
+
+        // Move backward
+        if (Input.GetKey("s"))
+        {
+            transform.Translate(Vector3.back * (Time.deltaTime * movementSpeed));
+        }
+
+        // Rotate left
+        if (Input.GetKey("a"))
+        {
+            transform.Rotate(0, -rotateSpeed * Time.deltaTime, 0);
+        }
+
+        // Rotate right
+        if (Input.GetKey("d"))
+        {
+            transform.Rotate(0, rotateSpeed * Time.deltaTime, 0);
+        }
+
+        // Jump upwards by adding force and check if it is on the ground
+        if (Input.GetKeyDown("space") && IsGrounded())
+        {
+            playerRB.AddForce(transform.up * (jumpHeight));
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            Debug.Log("Left Click");
+        }
+
+        if (Input.GetMouseButton(1))
+        {
+            Debug.Log("Right Click");
+        }
+    }
+
+    
+    // Checks if the collider is on the ground
+   bool IsGrounded(){
+    return Physics.Raycast(transform.position, -Vector3.up, distToGround + 1);
+ }
+
+
+}
